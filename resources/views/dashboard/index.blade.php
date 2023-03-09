4804085f2a9020f497e6467f5e856e30527c35a7
Derik Vila <derikvila@gmail.com>
2023-03-09 17:53:23 -0400

    master

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.dashboard')
+
+@section('title')
+Início
+@endsection
+
+@section('menu')
+basic minimal pushable
+@endsection
+
+
+@section('content')
+<br>
+
+@foreach($result as $dados)
+
+@endforeach
+
+
+<h1 class="display-3" style="text-align: center; font-size: 50px; padding-right: 5%; padding-left: 5%;">Início</h1>
+
+
+<form class="ui form container" action="/dashboard" method="POST" enctype="multipart/form-data">
+    <h1>Para habilitar ou desabilitar o sistema de matricula use o botão abaixo</h1>
+    @csrf
+    <button class="ui button"  id="botao-enviar" type="submit"> {{$dados ? 'HABILITAR' : 'DESABILITAR'}}</button>
+
+  </form>
+@endsection
+ 
